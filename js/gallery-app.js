4c3432da983cbcc50fb1fc8d5d8353bba0a7fc92
xpf0000 <xpf_163@163.com>
2016-08-01 15:37:31 +0800

    +++

---
+++ js/gallery-app.js
@@ -0,0 +1,31 @@
+$(function(){
+	var gallery = $('.swiper-container').swiper({
+		slidesPerView:'auto',
+		watchActiveIndex: true,
+		centeredSlides: true,
+		pagination:'.pagination',
+		paginationClickable: true,
+		resizeReInit: true,
+		keyboardControl: true,
+		grabCursor: true,
+		onImagesReady: function(){
+			changeSize()
+		}
+	})
+	function changeSize() {
+		//Unset Width
+		$('.swiper-slide').css('width','')
+		//Get Size
+		var imgWidth = $('.swiper-slide img').width();
+		if (imgWidth+40>$(window).width()) imgWidth = $(window).width()-40;
+		//Set Width
+		$('.swiper-slide').css('width', imgWidth+40);
+	}
+	
+	changeSize();	
+	console.log("\u767e\u5ea6\u641c\u7d22\u3010\u7d20\u6750\u5bb6\u56ed\u3011\u4e0b\u8f7d\u66f4\u591aJS\u7279\u6548\u4ee3\u7801");
+	$(window).resize(function(){
+		changeSize()
+		gallery.resizeFix(true)	
+	})
+})
